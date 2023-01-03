389733e9643d888fa0038603349b41891c2e387d
lautaro <lautarorondan96@gmail.com>
2023-01-03 01:49:47 -0300

    update index

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 const puerto =  process.env.PORT ||3000;
 
 // configurar cors
-app.use(cors({credentials:true}));
+app.use(cors());
 
 // convertir body a objeto js
 app.use(express.json());
